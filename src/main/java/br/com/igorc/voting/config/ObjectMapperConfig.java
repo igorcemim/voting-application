4bec2587402724f82036e25c736bdfa489d7bf8e
Igor Ferreira Cemim <igor@cemim.com.br>
2020-05-14 06:35:05 -0300

    Adicionada customização para a configuração do ObjectMapper

---
+++ src/main/java/br/com/igorc/voting/config/ObjectMapperConfig.java
@@ -0,0 +1,15 @@
+package br.com.igorc.voting.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@Configuration
+public class ObjectMapperConfig implements Jackson2ObjectMapperBuilderCustomizer {
+
+    @Override
+    public void customize(Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder) {
+        jacksonObjectMapperBuilder.featuresToEnable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL);
+    }
+}
